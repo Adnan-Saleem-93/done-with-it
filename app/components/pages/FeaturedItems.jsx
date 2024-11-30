f61c3d22c3278714eb04bb36a7873609ffeda710
Adnan Saleem <adnan13893@gmail.com>
2024-11-30 22:53:08 +0500

    added pages for homescreen sections

---
+++ app/components/pages/FeaturedItems.jsx
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native'
+import React from 'react'
+
+const FeaturedItems = () => {
+  return (
+    <View>
+      <Text>FeaturedItems</Text>
+    </View>
+  )
+}
+
+export default FeaturedItems
---
+++ app/components/pages/TastyDiscountsPage.jsx
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native'
+import React from 'react'
+
+const TastyDiscountsPage = () => {
+  return (
+    <View>
+      <Text>TastyDiscountsPage</Text>
+    </View>
+  )
+}
+
+export default TastyDiscountsPage
